460fc3773cf1def797822dd37fcb1ed45391556f
Brian Jepson <brian.jepson@raspberrypi.com>
2026-02-26 14:43:09 -0500

    tightened up code a bit

--- code/ch03/collect_cpu_data.py
+++ code/ch03/collect_cpu_data.py
@@ -1,26 +1,25 @@
 import pandas as pd
 import psutil
-import os
-import re
-import time
-from datetime import datetime
-from tqdm import trange
+import os # for os.popen to run vcgencmd commands
+import re # for regex to extract numbers from vcgencmd output
+import time # for time.sleep to control sampling rate
+from tqdm import trange # for progress bar
 
 # Collect some CPU activity/thermal data
 # Intuitively, we all know these are related.
 # But can some of these predict the others? Reliably?
 
-# vgencmd uses a format like "temp=45.2'C", so we need to
+# vcgencmd uses a format like "temp=45.2'C", so we need to
 # extract the numbers with decimal points if any.
 def extract_number(s):
     match = re.search(r"=(\d+(\.\d+)?)", s)
     return round(float(match.group(1)), 4)
 
-def cpu_speed():
+def speed():
     val = os.popen("vcgencmd measure_clock arm").read()
     return extract_number(val) / 10**9
 
-def cpu_temp():
+def temp():
     val = os.popen("vcgencmd measure_temp").read()
     return extract_number(val)
 
@@ -28,20 +27,22 @@ def volts():
     val = os.popen("vcgencmd measure_volts").read()
     return extract_number(val)
 
-numsamples = 2500
-samples = []
-pbar = trange(numsamples)
+if __name__ == "__main__":
+    numsamples = 2500
+    samples = []
+    pbar = trange(numsamples)
 
-for i in pbar:
-    cpu_pct = psutil.cpu_percent()
-    load_avg = round(psutil.getloadavg()[0], 2)
-    r = [cpu_speed(), volts(), cpu_pct, load_avg, cpu_temp()]
+    for i in pbar:
+        cpu_pct = psutil.cpu_percent()
+        load_avg = round(psutil.getloadavg()[0], 2)
+        r = [speed(), volts(), cpu_pct, load_avg, temp()]
 
-    samples.append(r)
-    pbar.set_description(
-        f"{r[0]}GHz {r[1]}V {r[2]}% {r[3]} {r[4]}C")
-    time.sleep(0.25)
+        samples.append(r)
+        pbar.set_description(
+            f"{r[0]}GHz {r[1]}V {r[2]}% {r[3]} {r[4]}C")
+        time.sleep(0.25)
 
-df = pd.DataFrame(samples)
-df.columns = ['cpu_speed', 'volts', 'cpu_pct', 'load_avg', 'temp']
-df.to_csv(f"readings.csv", index=False)
+    df = pd.DataFrame(samples)
+    df.columns = ['cpu_speed', 'volts', 'cpu_pct',
+                  'load_avg', 'temp']
+    df.to_csv(f"readings.csv", index=False)
