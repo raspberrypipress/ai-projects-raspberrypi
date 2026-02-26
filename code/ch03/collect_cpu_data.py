aa531c6e8c6fb870f2afb4af45ed3f0b054e1ba5
Brian Jepson <brian.jepson@raspberrypi.com>
2026-02-26 13:15:21 -0500

    more tightening of lines

--- code/ch03/collect_cpu_data.py
+++ code/ch03/collect_cpu_data.py
@@ -10,33 +10,38 @@ from tqdm import trange
 # Intuitively, we all know these are related.
 # But can some of these predict the others? Reliably?
 
-# vgencmd returns results in a format like "frequency(0)=1500019456",
-# "volt=0.8898V", or "temp=45.2'C" so we need to extract the numbers.
+# vgencmd uses a format like "temp=45.2'C", so we need to
+# extract the numbers with decimal points if any.
 def extract_number(s):
     match = re.search(r"=(\d+(\.\d+)?)", s)
     return round(float(match.group(1)), 4)
 
-def get_cpu_speed():
+def cpu_speed():
     val = os.popen("vcgencmd measure_clock arm").read()
     return extract_number(val) / 10**9
 
-def get_cpu_temp():
+def cpu_temp():
     val = os.popen("vcgencmd measure_temp").read()
     return extract_number(val)
 
-def get_volts():
+def volts():
     val = os.popen("vcgencmd measure_volts").read()
     return extract_number(val)
 
 numsamples = 2500
 samples = []
 pbar = trange(numsamples)
+
 for i in pbar:
-    r = [get_cpu_speed(), get_volts(), psutil.cpu_percent(), psutil.getloadavg()[0], get_cpu_temp()]
+    cpu_pct = psutil.cpu_percent()
+    load_avg = round(psutil.getloadavg()[0], 2)
+    r = [cpu_speed(), volts(), cpu_pct, load_avg, cpu_temp()]
+
     samples.append(r)
+    pbar.set_description(
+        f"{r[0]}GHz {r[1]}V {r[2]}% {r[3]} {r[4]}C")
     time.sleep(0.25)
-    pbar.set_description(f"{r[0]}GHz {r[1]}V {r[2]}% {r[3]} {r[4]}C ")
 
 df = pd.DataFrame(samples)
 df.columns = ['cpu_speed', 'volts', 'cpu_pct', 'load_avg', 'temp']
-df.to_csv(f"readings-{datetime.today().strftime('%Y-%m-%d-%H.00')}.csv", index=False)
+df.to_csv(f"readings.csv", index=False)
