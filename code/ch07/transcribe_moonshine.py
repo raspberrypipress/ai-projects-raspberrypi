d8b22804be13f1fc5ad0a31e6df0901a99a9e906
Brian Jepson <bjepson@gmail.com>
2026-04-12 15:07:22 -0400

    removed blocksize override

--- code/ch07/transcribe_moonshine.py
+++ code/ch07/transcribe_moonshine.py
@@ -20,7 +20,6 @@ if __name__ == "__main__":
     options = {"return_audio_data": False}
     mic_transcriber = MicTranscriber(model_path=model_path,
                                      model_arch=model_arch,
-                                     blocksize=4096,
                                      options=options)
 
     listener = FileListener()
