5d8b88c055b3918b728a0578be9fcbadc8d549e1
Brian Jepson <bjepson@gmail.com>
2026-04-12 17:34:35 -0400

    Added some comments, streamlined the code a bit

--- code/ch07/transcribe_moonshine.py
+++ code/ch07/transcribe_moonshine.py
@@ -4,6 +4,7 @@ from moonshine_voice.transcriber import TranscriptEventListener
 import time
 import sys
 
+# Define a listener to display completed lines of transcription.
 class FileListener(TranscriptEventListener):
     def on_line_completed(self, event):
         print(event.line.text)
@@ -11,28 +12,29 @@ class FileListener(TranscriptEventListener):
 
 if __name__ == "__main__":
 
-    lang = "en"
-    model = ModelArch.TINY_STREAMING
+    # Load the model for the language we want to transcribe.
     model_path, model_arch = get_model_for_language(
-        wanted_language=lang, wanted_model_arch=model
+        "en", ModelArch.TINY_STREAMING
     )
 
+    # Configure the transcriber.
     options = {"return_audio_data": False, 
                "identify_speakers": False}
     mic_transcriber = MicTranscriber(model_path=model_path,
                                      model_arch=model_arch,
                                      options=options)
 
-    listener = FileListener()
-    mic_transcriber.add_listener(listener)
-
-    print(f"Ctrl+C to stop...", file=sys.stderr)
+    # Add the listener to the transcriber, and start it.
+    mic_transcriber.add_listener(FileListener())
     mic_transcriber.start()
+
+    # Keep running until the user presses Ctrl+C.
+    print(f"Ctrl+C to stop...", file=sys.stderr)
     try:
         while True:
             time.sleep(0.1)
     except KeyboardInterrupt:
-        print("Finished!", file=sys.stderr)
+        print("Finished.", file=sys.stderr)
     finally:
         mic_transcriber.stop()
         mic_transcriber.close()
