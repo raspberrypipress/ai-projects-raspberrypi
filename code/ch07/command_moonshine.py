bb3630fff793e202eb5a70f13a7342af1acb4d48
Brian Jepson <bjepson@gmail.com>
2026-04-13 17:31:12 -0400

    initial import of command recogniser

---
+++ code/ch07/command_moonshine.py
@@ -0,0 +1,61 @@
+from moonshine_voice import (
+    IntentRecognizer,
+    MicTranscriber,
+    ModelArch,
+    get_embedding_model,
+    get_model_for_language,
+)
+import time
+import sys
+from gpiozero import LED
+
+led = LED(3)
+def led_on():
+    led.on()
+    print("LED turned on.")
+def led_off():
+    led.off()
+    print("LED turned off.")
+
+if __name__ == "__main__":
+
+    embedding_model_path, embedding_model_arch = get_embedding_model(
+        "embeddinggemma-300m", "q4"
+    )
+
+    intent_recognizer = IntentRecognizer(
+        model_path=embedding_model_path,
+        model_arch=embedding_model_arch,
+        model_variant="q4",
+        threshold=0.6,
+    )
+
+    intent_recognizer.register_intent("turn on the L E D", led_on)
+    intent_recognizer.register_intent("turn off the L E D", led_off)
+
+    # Load the model for the language we want to transcribe.
+    model_path, model_arch = get_model_for_language(
+        "en", ModelArch.TINY_STREAMING
+    )
+
+    # Configure the transcriber.
+    options = {"return_audio_data": False, 
+               "identify_speakers": False}
+    mic_transcriber = MicTranscriber(model_path=model_path,
+                                     model_arch=model_arch,
+                                     options=options)
+
+    # Add the recognizer to the transcriber, and start it.
+    mic_transcriber.add_listener(intent_recognizer)
+    mic_transcriber.start()
+
+    # Keep running until the user presses CTRL+C.
+    print(f"CTRL+C to stop...", file=sys.stderr)
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("Finished.", file=sys.stderr)
+    finally:
+        mic_transcriber.stop()
+        mic_transcriber.close()
