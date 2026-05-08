0a6a37c8bc79af325d527b303168495d5a77a812
Brian Jepson <bjepson@gmail.com>
2026-05-08 14:24:57 -0400

    made translation example more robust

--- code/ch07/translate_opus.py
+++ code/ch07/translate_opus.py
@@ -1,23 +1,34 @@
 from transformers import MarianMTModel, MarianTokenizer
-import time
 import sys
+from nltk.tokenize import sent_tokenize
 
 if __name__ == "__main__":
 
+    # Load the model and tokenizer.
     mt_model_name = "Helsinki-NLP/opus-mt-en-de"
     tokenizer = MarianTokenizer.from_pretrained(mt_model_name)
     mt_model = MarianMTModel.from_pretrained(mt_model_name)
 
     print(f"CTRL+C to stop...", file=sys.stderr)
     try:
-        for line in sys.stdin:
-            inputs = tokenizer(line.strip(),
+        for text in sys.stdin:
+            # Break the input text into sentences.
+            sentences = sent_tokenize(text)
+
+            # Translate the sentences as a batch.
+            inputs = tokenizer(sentences,
                                return_tensors="pt",
                                padding=True,
-                               truncation=True)
+                               truncation=True,
+                               )
             translated = mt_model.generate(**inputs)
-            r = tokenizer.decode(translated, 
-                                 skip_special_tokens=True)
-            print(r[0])
+
+            # Decode and print the translated sentences.
+            decoded = tokenizer.decode(translated, 
+                                       skip_special_tokens=True
+                                      )
+            for sentence in decoded:
+                print(sentence)
+
     except KeyboardInterrupt:
         print("Finished!", file=sys.stderr)
