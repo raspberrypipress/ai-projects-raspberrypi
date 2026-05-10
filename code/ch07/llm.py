870497c30bd6cd4cb92b36103089f15f417ae2b7
Brian Jepson <bjepson@gmail.com>
2026-05-10 17:25:37 -0400

    made more pipe-friendly

--- code/ch07/llm.py
+++ code/ch07/llm.py
@@ -12,7 +12,7 @@ import sys
 if __name__ == "__main__":
 
     hef_path = Path.home() / "Downloads" / "Qwen3-1.7B-Instruct.hef"
-    print(f"Using model {hef_path}")
+    print(f"Using model {hef_path}", file=sys.stderr)
 
     vdevice = None
     llm = None
@@ -20,32 +20,29 @@ if __name__ == "__main__":
         params = VDevice.create_params()
         params.group_id = SHARED_VDEVICE_GROUP_ID
         vdevice = VDevice(params)
-        print("Hailo device initialized")
+        print("Hailo device initialized", file=sys.stderr)
 
         llm = LLM(vdevice, str(hef_path))
-        print("Model loaded successfully")
+        print("Model loaded successfully", file=sys.stderr )
 
         messages = [message_formatter.messages_system("You are a helpful assistant.")]
         while True:
-            user_input = input("> ")
+            user_input = input()
             if user_input.lower() in ["quit", "exit", "bye"]:
                 break
             messages.append(message_formatter.messages_user(user_input))
 
             response = llm.generate_all(prompt=messages, temperature=0.1, seed=42, max_generated_tokens=512)
 
-            print("-" * 60)
             cleaned_response = response.split(". [{'type'")[0]
             cleaned_response = cleaned_response.replace("<|im_end|>", "")
-            print(f"Response: {cleaned_response}")
+            print(cleaned_response)
             messages.append(message_formatter.messages_assistant(cleaned_response))
             context_manager.print_context_usage(llm, show_always=True)
-            print(messages)
-            print("-" * 60)
-        print("Done")
+        print("Farewell from llm.py", file=sys.stderr)
 
     except Exception as e:
-        print(f"Error occurred: {e}")
+        print(f"Error occurred: {e}", file=sys.stderr)
         sys.exit(1)
 
     finally:
