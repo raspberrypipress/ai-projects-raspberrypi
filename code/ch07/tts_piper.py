1620b22ccbe504d05d25e94538071d191776805d
Brian Jepson <bjepson@gmail.com>
2026-05-12 08:20:54 -0400

    warmup

--- code/ch07/tts_piper.py
+++ code/ch07/tts_piper.py
@@ -1,5 +1,6 @@
 import signal
 import sys
+from time import time
 from piper import PiperVoice
 import numpy as np
 import sounddevice as sd
@@ -8,6 +9,7 @@ voice = PiperVoice.load("./en_US-lessac-medium.onnx")
 stream = sd.OutputStream(samplerate=voice.config.sample_rate,
                          channels=1, dtype='int16')
 stream.start()
+time.sleep(0.1)  # Give the stream a moment to start
 
 signal.signal(signal.SIGINT, signal.SIG_IGN)
 print(f"Ready.", file=sys.stderr)
