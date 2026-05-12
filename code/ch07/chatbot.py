21e74e298c8b674758617d29539bee08ebc04fdb
Brian Jepson <bjepson@gmail.com>
2026-05-12 17:06:58 -0400

    more muting

--- code/ch07/chatbot.py
+++ code/ch07/chatbot.py
@@ -23,8 +23,12 @@ def say(text):
 
 class FileListener(TranscriptEventListener):
     def on_line_completed(self, event):
+        mic_transcriber._should_listen = False
         diags.print(f"Transcribed: {event.line.text}")
-        say(llm.generate(event.line.text))
+        response = llm.generate(event.line.text)
+        diags.print(f"LLM response: {response}")
+        say(response)
+        mic_transcriber._should_listen = True
 
 
 # Load the model for the language we want to transcribe.
