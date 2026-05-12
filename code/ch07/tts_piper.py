346d87e070acc9cbf042dfe08672c9f4668d05cd
Brian Jepson <bjepson@gmail.com>
2026-05-12 16:15:12 -0400

    first class-ed up version

--- code/ch07/tts_piper.py
+++ code/ch07/tts_piper.py
@@ -5,20 +5,32 @@ from piper import PiperVoice
 import numpy as np
 import sounddevice as sd
 
-voice = PiperVoice.load("./en_US-lessac-medium.onnx")
-stream = sd.OutputStream(samplerate=voice.config.sample_rate,
-                         channels=1, dtype='int16')
-stream.start()
-time.sleep(0.5)  # Give the stream a moment to start
+class TTSApp:
 
-signal.signal(signal.SIGINT, signal.SIG_IGN)
-print(f"Ready.", file=sys.stderr)
-for text in sys.stdin:
-    for chunk in voice.synthesize(text):
-        int_data = np.frombuffer(chunk.audio_int16_bytes,
-                                 dtype=np.int16)
-        stream.write(int_data)
+    def __init__(self, model):
+        self.voice = PiperVoice.load(model)
+        sample_rate = self.voice.config.sample_rate
+        self.stream = sd.OutputStream(samplerate=sample_rate,
+                                     channels=1, dtype='int16')
+        self.stream.start()
+        time.sleep(0.5)  # Give the stream a moment to start
 
-stream.stop()
-stream.close()
-print(f"Text-to-speech completed.", file=sys.stderr)
+    def speak(self, text):
+        for chunk in self.voice.synthesize(text):
+            int_data = np.frombuffer(chunk.audio_int16_bytes,
+                                     dtype=np.int16)
+        self.stream.write(int_data)
+    
+    def __del__(self):
+        self.stream.stop()
+        self.stream.close()
+
+if __name__ == "__main__":
+    app = TTSApp("./en_US-lessac-medium.onnx")
+
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    print(f"Ready.", file=sys.stderr)
+    for text in sys.stdin:
+        app.speak(text.strip())
+
+    print(f"Text-to-speech completed.", file=sys.stderr)
