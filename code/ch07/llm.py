dcdad22e77a3096703f558ef71ae03d2adc56ecc
Brian Jepson <bjepson@gmail.com>
2026-05-12 14:38:11 -0400

    style

--- code/ch07/llm.py
+++ code/ch07/llm.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import sys
 import signal
 
-diagnostics = Console(stderr=True, style="dark_red")
+diagnostics = Console(stderr=True, style="deep_pink4")
 output = Console(style="cornflower_blue")
 
 model_name = "Qwen2-1.5B-Instruct"
