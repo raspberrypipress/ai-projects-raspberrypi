9a9d91917b101e5a8dbbfdfce77e07aeece29985
Brian Jepson <bjepson@gmail.com>
2026-05-13 13:33:55 -0400

    Will this work without hailo-apps repo?

--- code/ch07/llm.py
+++ code/ch07/llm.py
@@ -1,14 +1,5 @@
 from hailo_platform import VDevice
 from hailo_platform.genai import LLM
-from hailo_apps.python.gen_ai_apps.gen_ai_utils.llm_utils \
-    import (
-        message_formatter,
-        streaming,
-        agent_utils,
-        context_manager,
-    )
-from hailo_apps.python.core.common.defines \
-    import SHARED_VDEVICE_GROUP_ID
 from rich.console import Console
 from pathlib import Path
 import sys
@@ -17,7 +8,6 @@ import signal
 class LLMApp:
 
     def __init__(self, model, console, prompt):
-
         self.console = console
 
         hef_path = Path.home() / "Downloads" / f"{model}.hef"
@@ -25,7 +15,7 @@ class LLMApp:
 
         self.console.print("Initialising device...")
         params = VDevice.create_params()
-        params.group_id = SHARED_VDEVICE_GROUP_ID
+        params.group_id = "SHARED"
         self.vdevice = VDevice(params)
 
         self.console.print("Loading model...")
@@ -33,19 +23,24 @@ class LLMApp:
 
         # Add system prompt to the LLM's context.
         self.console.print("Initialising model...")
-        sys_message = message_formatter.messages_system(prompt)
-        context_manager.add_to_context(self.llm, [sys_message])
-        self.sys_message = sys_message
+        self.sysmsg = {"role": "system", "content": prompt}
+        self.initialize_context()
+
+    def initialize_context(self):
+        self.llm.clear_context()
+        for token in self.llm.generate(prompt=[self.sysmsg],
+                                       max_generated_tokens=1):
+            pass
 
     def generate(self, user_input):
+        capacity = self.llm.max_context_capacity()
+        usage = self.llm.get_context_usage_size()
 
-        if context_manager.is_context_full(self.llm, 0.90):
+        if usage / capacity > 0.90:
             self.console.print("Context full, clearing.")
-            self.llm.clear_context()
-            context_manager.add_to_context(self.llm,
-                                           [self.sys_message])
+            self.initialize_context()
 
-        msg = message_formatter.messages_user(user_input)
+        msg = {"role": "user", "content": user_input}
         r = ""
         try:
             with self.console.status("[blue]Generating..."):
@@ -59,7 +54,9 @@ class LLMApp:
         return streaming.clean_response(r)
 
     def __del__(self):
-        agent_utils.cleanup_resources(self.llm, self.vdevice)
+        self.llm.clear_context()
+        self.llm.release()
+        self.vdevice.release()
 
 if __name__ == "__main__":
     # Set up some rich consoles for diags and output.
