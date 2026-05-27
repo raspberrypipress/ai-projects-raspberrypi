ad888de2952659af0eb8ec9354e3eb567d3910a6
Brian Jepson <bjepson@gmail.com>
2026-05-27 16:43:11 +0100

    new files

---
+++ ch04/astro-horse-SDXL.py
@@ -0,0 +1,17 @@
+import os # We need this to expand ~ to your actual home dir
+from diffusers import StableDiffusionXLPipeline
+import torch # we need this to set the float
+
+# The expanded path to the model
+model = os.path.expanduser("~/Models/stable-diffusion-xl-base-1.0")
+
+# Load the model
+pipe = StableDiffusionXLPipeline.from_pretrained(model,
+                                  torch_dtype=torch.float32,
+                                  low_cpu_mem_usage=True)
+pipe = pipe.to("cpu") # Move the model to the CPU
+prompt = "an astronaut riding a horse" # Set the prompt
+image = pipe(prompt, num_inference_steps=5,
+             width=1024, height=1024).images[0]
+
+image.save("astro-horse_SDXL.png") 
--- ch04/astro-horse35.py
+++ ch04/astro-horse35.py
@@ -11,7 +11,7 @@ pipe = StableDiffusionPipeline.from_pretrained(model,
 pipe = pipe.to("cpu") # Move the model to the CPU
 
 prompt = "an astronaut riding a horse" # Set the prompt
-image = pipe(prompt, num_inference_steps=5, 
+image = pipe(prompt, num_inference_steps=35, 
              width=640, height=640).images[0]
 
 import pdb; pdb.set_trace() # Set a breakpoint here to inspect the image variable
