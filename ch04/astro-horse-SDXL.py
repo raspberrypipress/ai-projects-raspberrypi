1b7ed4623e0523f301c2fa35a09772d6bb6ee47c
Brian Jepson <bjepson@gmail.com>
2026-05-28 13:47:59 +0100

    minor edits

--- ch04/astro-horse-SDXL.py
+++ ch04/astro-horse-SDXL.py
@@ -2,12 +2,9 @@ import os # We need this to expand ~ to your actual home dir
 from diffusers import StableDiffusionXLPipeline
 import torch # we need this to set the float
 
-# Get the expanded path to the model
-model_name = "stable-diffusion-xl-base-1.0"
-model = os.path.expanduser(f"~/Models/{model_name}")
-
 # Load the model
-pipe = StableDiffusionXLPipeline.from_pretrained(model,
+model_name = "stabilityai/stable-diffusion-xl-base-1.0"
+pipe = StableDiffusionXLPipeline.from_pretrained(model_name,
                                   torch_dtype=torch.float32,
                                   low_cpu_mem_usage=True)
 pipe = pipe.to("cpu") # Move the model to the CPU
@@ -15,4 +12,4 @@ prompt = "an astronaut riding a horse" # Set the prompt
 image = pipe(prompt, num_inference_steps=5,
              width=1024, height=1024).images[0]
 
-image.save("astro-horse_SDXL.png")
+image.save("astro-horse-SDXL.png")
