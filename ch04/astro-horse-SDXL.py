045d544079d4750bf6f3a4c6146233f78efea56a
Brian Jepson <bjepson@gmail.com>
2026-05-28 12:02:41 +0100

    minor edit

--- ch04/astro-horse-SDXL.py
+++ ch04/astro-horse-SDXL.py
@@ -10,8 +10,11 @@ pipe = StableDiffusionXLPipeline.from_pretrained(model,
                                   torch_dtype=torch.float32,
                                   low_cpu_mem_usage=True)
 pipe = pipe.to("cpu") # Move the model to the CPU
+
 prompt = "an astronaut riding a horse" # Set the prompt
-image = pipe(prompt, num_inference_steps=5,
+neg_prompt = "blurry"
+image = pipe(prompt, negative_prompt=neg_prompt,
+             num_inference_steps=5,
              width=1024, height=1024).images[0]
 
 image.save("astro-horse-SDXL.png")
