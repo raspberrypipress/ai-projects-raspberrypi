0580321bca82178459aec8cfc039e7de2d99473e
Brian Jepson <bjepson@gmail.com>
2026-05-30 20:11:11 -0400

    more edits

--- ch05/extract_tokens_from_prompt.py
+++ ch05/extract_tokens_from_prompt.py
@@ -6,13 +6,19 @@ messages = [
 ]
 input_ids = tokenizer.apply_chat_template(
     messages,
-    add_generation_prompt=False,
-    return_tensors="pt"
+    add_generation_prompt=True,
 )
-decoded_tokens = [tokenizer.decode([token_id]) for token_id in input_ids["input_ids"][0]]
+print("Input IDs:", input_ids["input_ids"])
+decoded_tokens = [tokenizer.decode([token_id]) for token_id in input_ids["input_ids"]]
 
 # Print them one by one with their id
-i = 1
-for id, token in zip(input_ids["input_ids"][0], decoded_tokens):
-    print(f"Token #{i:02d} ID {id:<6}: {repr(token)}")
+i = 0
+for id, token in zip(input_ids["input_ids"], decoded_tokens):
+    print(f"Token #{i:02d} ID {id:>7}: {repr(token)}")
     i += 1
+
+
+# ollama prompt from the ollama log
+"""
+prompt="<|start_header_id|>user<|end_header_id|>\n\nexplain quantum computing to me like I'm a five year old<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+"""
