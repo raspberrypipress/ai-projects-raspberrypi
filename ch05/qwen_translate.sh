cfb35567a26e15b8b586b8f1f8107a3285636a17
Brian Jepson <bjepson@gmail.com>
2026-05-31 18:52:05 -0400

    shell example

---
+++ ch05/qwen_translate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+curl --silent http://localhost:8000/api/chat \
+     -H 'Content-Type: application/json' \
+     -d @- <<EOF
+{"model": "qwen2:1.5b",
+ "messages": [
+   {"role": "user",
+    "content": "Translate to French: The cat is on the table."
+   }]}
+EOF
