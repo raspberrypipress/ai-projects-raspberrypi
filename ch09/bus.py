b0500afddb480617d35c159bf4a47f15685284e0
Brian Jepson <brian.jepson@raspberrypi.com>
2026-06-03 10:54:01 -0400

    task

--- ch09/bus.py
+++ ch09/bus.py
@@ -1,11 +1,10 @@
 from ultralytics import YOLO
 
 # Load a pretrained YOLO model
-model = YOLO("yolo26n_ncnn_model")
+model = YOLO("yolo26n_ncnn_model", task="detect")
 
 # Perform object detection on an image
-results = model("https://ultralytics.com/images/bus.jpg",
-                task="detect")
+results = model("https://ultralytics.com/images/bus.jpg")
 
 # Visualize the results
 for result in results:
