baec083038dda00a7c1a44fb6e37fd9e83e448d2
Brian Jepson <bjepson@gmail.com>
2026-06-11 09:06:29 -0400

    changed the size of the 2d tensors to something less ambiguous

--- ch01/tensor_tf.py
+++ ch01/tensor_tf.py
@@ -10,8 +10,8 @@ vector = tf.constant([1, 2, 3])
 print(vector.shape) # Output: (3,)
 
 # Creating a 2D tensor (matrix)
-matrix = tf.constant([[1, 2], [3, 4]])
-print(matrix.shape) # Output: (2, 2)
+matrix = tf.constant([[1, 2, 3], [4, 5, 6]])
+print(matrix.shape) # Output: (2, 3)
 
 # Creating a 3D tensor (e.g., image with 3 colour channels)
 # Produces a 128x128x3 tensor of values between 0 and 1
